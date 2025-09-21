1e02ff79d9c07def9e06fa2e13374b98851b4f0c
JeromeMingalaSectionD <n01687151@humber.ca>
2025-09-21 13:00:18 -0400

    Lab2 completed: env config, scripts, docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-native', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'error',
+  },
 };
---
+++ PROJECT_STRUCTURE.md
@@ -0,0 +1,26 @@
+# Project Structure
+
+## Folders
+- src/components
+- src/screens
+- src/services
+- src/utils
+- src/constants
+- src/hooks
+- src/context
+- src/styles
+- src/assets/images
+- src/assets/fonts
+
+## Scripts
+- npm run lint
+- npm run lint:fix
+- npm run format
+- npm run android
+- npm run dev:android
+- npm run clean
+
+## Environment
+- .env
+- .env.development
+- .env.production
